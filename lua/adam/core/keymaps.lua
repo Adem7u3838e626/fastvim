568a994e3a7ce25ae6961b0ec1993aef9dff7dd7
Adam <alouati559@gmail.com>
2026-05-21 13:26:21 +0100

    change bufferline kymaps && clean keymaps
    remove tab keymab and replased it with same new maps and remove the
    oldest terminal binds from the keymaps

--- lua/adam/core/keymaps.lua
+++ lua/adam/core/keymaps.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
-vim.opt.shell = "/bin/bash"
 
 -- Exit insert mode
 keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
@@ -22,18 +21,9 @@ keymap.set("v", "p", '"_dP', { desc = "Paste without overwrite yank" })
 -- Disable Q
 keymap.set("n", "Q", "<nop>")
 
--- Bufferline
-keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", { desc = "Close buffer" })
-keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-
 -- Splits
 keymap.set("n", "<leader>sf", ":vsplit<CR>", { noremap = true, silent = true })
 keymap.set("n", "<leader>sv", ":split<CR>", { noremap = true, silent = true })
--- Terminal splits
-keymap.set("n", "<Leader>sft", ":botright split | resize 15 | terminal<CR>", { noremap = true, silent = true })
-keymap.set("n", "<Leader>svt", ":vsplit | terminal<CR>", { noremap = true, silent = true })
-keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 -- Resize splits with Ctrl+Shift+HJKL
 vim.keymap.set("n", "<C-S-h>", "<C-w><")
 vim.keymap.set("n", "<C-S-l>", "<C-w>>")
@@ -42,10 +32,6 @@ vim.keymap.set("n", "<C-S-j>", "<C-w>-")
 vim.keymap.set("n", "<leader>=", "<C-w>=", { desc = "Balance windows" })
 -- Close current split/window
 keymap.set("n", "<leader>cc", ":close<CR>", { noremap = true, silent = true, desc = "Close current window" })
--- Navigate windows using leader+Tab if needed
-keymap.set("n", "<leader><Tab>", "<C-w>w", { noremap = true, silent = true })
-keymap.set("n", "<leader><S-Tab>", "<C-w>W", { noremap = true, silent = true })
-
 
 keymap.set("n", "<leader>qw", ":wq!<CR>", vim.tbl_extend("force", opts, { desc = "Quit with saving" }))
 keymap.set("n", "<leader>Q", ":qa!<CR>", vim.tbl_extend("force", opts, { desc = "Quit all without saving" }))
--- lua/adam/plugins/bufferline.lua
+++ lua/adam/plugins/bufferline.lua
@@ -1,12 +1,17 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    opts = {
-      options = {
-        numbers = "tabs",
-        separator_style = "slant",
-      },
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      numbers = "ordinal",
+      separator_style = "slant",
+      diagnostics = "nvim_lsp",
+      show_buffer_close_icons = false,
+      show_close_icon = false,
     },
+  },
+  vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>", { desc = "Next buffer" })
+  vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>", { desc = "Prev buffer" })
+  vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
 }
-
